d652aa5ae134068b2a932e5d09b4ee6d41d7710b
drissiReda <mohamedreda.drissi@edu.uca.ma>
2018-03-29 17:29:24 +0200

    Added cleanup of converter string

--- report/resources/L1/warmup/ink.sh
+++ report/resources/L1/warmup/ink.sh
@@ -2,7 +2,7 @@
 for i in $(ls *.svg)
 do
     i=$(echo "$i"| sed 's/\.[^.]*$//' | sed 's/\.[^.]*$//')
-    convert -background none -size 1024x1024 $i.tsv.svg $i.png
+    convert -background none -size 1024x1024 $i.tsv.svg $i.png && rm $i.tsv $i.tsv.svg
 done
 
 
